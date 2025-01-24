e17dd4475943245d66ea4972d443b7be08dc9245
Earth4455 <wiwach.ad.67@ubu.ac.th>
2025-01-24 14:04:11 +0700

    สร้างคลาสแรกบนGithub repo 555

---
+++ app.py
@@ -0,0 +1,13 @@
+clss student(object):
+ student_count = 0
+
+ def _new_(self)
+   print('Student._new_')
+
+def _init_(self):
+  print('Student._init_')
+  self.gpa = 4.00
+
+def study_hard(self):
+  print('Sir, yes sir.')
+  
